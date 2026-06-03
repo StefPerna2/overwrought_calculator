01a53ee469eb2506e3bae839cad2aa643c8b06b8
StefPerna2 <stefano.perna@warwick.ac.uk>
2026-06-03 09:06:25 +0100

    Delete addition.R

--- addition.R
+++
@@ -1,9 +0,0 @@
-print("Please insert 1st number.")
-n1 <- as.integer(readline())
-print("Please insert second number.")
-n2 <- as.integer(readline())
-
-print(
-  paste("The sum of ",n1," and ",n2," is ",n1+n2,
-  sep="")
-)
