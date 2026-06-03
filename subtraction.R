11e855e16c7eebc0b28eaa98485aa46af13fc0fc
StefPerna2 <stefano.perna@warwick.ac.uk>
2026-06-03 09:06:34 +0100

    Delete subtraction.R

--- subtraction.R
+++
@@ -1,9 +0,0 @@
-print("Please insert first number.")
-n1 <- as.integer(readline())
-print("Please insert second number.")
-n2 <- as.integer(readline())
-
-print(
-  paste("The difference of ",n1," and ",n2," is ",n1-n2,
-        sep="")
-)
